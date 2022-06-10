a9c4b8d70c528e5b9be38d184b59da97cbf56fcc
Shuhuai Cao <csh980717@outlook.com>
2022-06-10 15:45:50 -0400

    Initial commit

---
+++ src/main/java/tech/tennoji/dronewatchserver/DroneWatchServerApplication.java
@@ -0,0 +1,13 @@
+package tech.tennoji.dronewatchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneWatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneWatchServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/tennoji/dronewatchserver/DroneWatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.tennoji.dronewatchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneWatchServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
