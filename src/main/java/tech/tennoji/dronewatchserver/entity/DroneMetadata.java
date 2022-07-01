ca7d7ca8cde6b2c866f88f03ec68b91f265beda7
Shuhuai Cao <csh980717@outlook.com>
2022-07-01 16:31:49 -0400

    Drone record entity

---
+++ src/main/java/tech/tennoji/dronewatchserver/entity/DroneMetadata.java
@@ -0,0 +1,10 @@
+package tech.tennoji.dronewatchserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DroneMetadata {
+    String droneId;
+}
