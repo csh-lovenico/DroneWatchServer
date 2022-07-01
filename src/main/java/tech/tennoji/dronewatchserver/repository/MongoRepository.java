4b1734964b6223d48a9e9640410c352443487cee
Shuhuai Cao <csh980717@outlook.com>
2022-07-01 16:32:39 -0400

    Add methods in mongo repository

--- src/main/java/tech/tennoji/dronewatchserver/repository/MongoRepository.java
+++ src/main/java/tech/tennoji/dronewatchserver/repository/MongoRepository.java
@@ -3,9 +3,11 @@ package tech.tennoji.dronewatchserver.repository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+import tech.tennoji.dronewatchserver.entity.GeoFence;
 import tech.tennoji.dronewatchserver.entity.Subscription;
 
 import java.util.List;
@@ -39,5 +41,23 @@ public class MongoRepository {
         query.addCriteria(Criteria.where("token").is(token));
         var result = mongoTemplate.remove(query, "subscription");
         return result.getDeletedCount();
+    }
+
+    public List<GeoFence> findFencesNear(long longitude, long latitude, long distance) {
+        GeoJsonPoint point = new GeoJsonPoint(longitude, latitude);
+        Query query = new Query();
+        query.addCriteria(Criteria.where("location").nearSphere(point).minDistance(0).maxDistance(distance));
+        return mongoTemplate.find(query, GeoFence.class, "fence");
+    }
+
+    public long checkIfNearFence(long longitude, long latitude, long distance, String name) {
+        GeoJsonPoint point = new GeoJsonPoint(longitude, latitude);
+        Query query = new Query();
+        query.addCriteria(Criteria.where("name").is(name).andOperator(Criteria.where("location").nearSphere(point).maxDistance(distance)));
+        return mongoTemplate.count(query, GeoFence.class, "fence");
+    }
+
+    public void addFence(GeoFence fence) {
+        mongoTemplate.insert(fence, "fence");
     }
 }
