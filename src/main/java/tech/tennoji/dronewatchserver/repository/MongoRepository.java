13b8b0f973261e33c5091358bd1372fe5921f188
Shuhuai Cao <csh980717@outlook.com>
2022-07-03 22:20:18 -0400

    Add methods for mongo operations

--- src/main/java/tech/tennoji/dronewatchserver/repository/MongoRepository.java
+++ src/main/java/tech/tennoji/dronewatchserver/repository/MongoRepository.java
@@ -2,11 +2,13 @@ package tech.tennoji.dronewatchserver.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+import tech.tennoji.dronewatchserver.entity.DroneRecord;
 import tech.tennoji.dronewatchserver.entity.GeoFence;
 import tech.tennoji.dronewatchserver.entity.Subscription;
 
@@ -43,14 +45,14 @@ public class MongoRepository {
         return result.getDeletedCount();
     }
 
-    public List<GeoFence> findFencesNear(long longitude, long latitude, long distance) {
+    public List<GeoFence> findFencesByLocation(long longitude, long latitude, long distance) {
         GeoJsonPoint point = new GeoJsonPoint(longitude, latitude);
         Query query = new Query();
         query.addCriteria(Criteria.where("location").nearSphere(point).minDistance(0).maxDistance(distance));
         return mongoTemplate.find(query, GeoFence.class, "fence");
     }
 
-    public long checkIfNearFence(long longitude, long latitude, long distance, String name) {
+    public long findFencesByNameAndLocation(long longitude, long latitude, long distance, String name) {
         GeoJsonPoint point = new GeoJsonPoint(longitude, latitude);
         Query query = new Query();
         query.addCriteria(Criteria.where("name").is(name).andOperator(Criteria.where("location").nearSphere(point).maxDistance(distance)));
@@ -60,4 +62,32 @@ public class MongoRepository {
     public void addFence(GeoFence fence) {
         mongoTemplate.insert(fence, "fence");
     }
+
+    public void addDroneRecord(DroneRecord record) {
+        mongoTemplate.insert(record, "drone_image_record");
+    }
+
+    public List<Subscription> findSubscriptionByToken(String token) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("token").is(token));
+        return mongoTemplate.find(query, Subscription.class, "subscription");
+    }
+
+    public List<GeoFence> findAllAreas() {
+        return mongoTemplate.findAll(GeoFence.class, "fence");
+    }
+
+    public DroneRecord findLatestDroneRecordByDrone(String droneId) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("droneId").is(droneId));
+        query.with(Sort.by(Sort.Direction.DESC, "timestamp"));
+        query.limit(1);
+        var result = mongoTemplate.find(query, DroneRecord.class, "drone_image_record");
+        if (!result.isEmpty()) {
+            return result.get(0);
+        } else {
+            return null;
+        }
+    }
+
 }
