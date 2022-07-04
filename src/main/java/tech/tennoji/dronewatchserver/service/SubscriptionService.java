b09560a94aa47eef90ab1558b3aec374be70f478
Shuhuai Cao <csh980717@outlook.com>
2022-07-03 22:21:05 -0400

    Add methods for managing subscriptions

--- src/main/java/tech/tennoji/dronewatchserver/service/SubscriptionService.java
+++ src/main/java/tech/tennoji/dronewatchserver/service/SubscriptionService.java
@@ -4,8 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import tech.tennoji.dronewatchserver.entity.GeoFence;
 import tech.tennoji.dronewatchserver.entity.Subscription;
 import tech.tennoji.dronewatchserver.repository.MongoRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -14,17 +18,33 @@ public class SubscriptionService {
     @Autowired
     private MongoRepository mongoRepository;
 
-    public void subscribeTopic(String token, String topic) {
+    public int subscribeTopic(String token, String topic) {
         try {
             mongoRepository.addSubscription(new Subscription(token, topic));
+            return 0;
         } catch (DuplicateKeyException e) {
             log.error("Cannot subscribe: Duplicate subscription.");
-        } catch (Exception e) {
-            log.error("Cannot subscribe: " + e.getMessage());
+            return 1;
         }
     }
 
     public void unsubscribeTopic(String token, String topic) {
         long count = mongoRepository.removeSubscription(token, topic);
+    }
+
+    public List<String> getSubscribedTopics(String token) {
+        var subscriptions = mongoRepository.findSubscriptionByToken(token);
+        return subscriptions.parallelStream().map(Subscription::getTopic).collect(Collectors.toList());
+    }
+
+    public List<String> getNotSubscribedTopics(String token) {
+        var allTopics = mongoRepository.findAllAreas().parallelStream().map(GeoFence::getName).collect(Collectors.toList());
+        var subscribedTopics = mongoRepository.findSubscribersByTopic(token).parallelStream().map(Subscription::getTopic).collect(Collectors.toList());
+        allTopics.removeAll(subscribedTopics);
+        return allTopics;
+    }
+
+    public long removeAllSubscriptionsByDevice(String token) {
+        return mongoRepository.removeDevice(token);
     }
 }
