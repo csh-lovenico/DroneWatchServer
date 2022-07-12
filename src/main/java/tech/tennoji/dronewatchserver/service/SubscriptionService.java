cbfe5b3a26dcec55b4d81bba656d8bb3b0fbaee6
Shuhuai Cao <csh980717@outlook.com>
2022-07-12 10:14:18 -0400

    Get area status & use serial stream

--- src/main/java/tech/tennoji/dronewatchserver/service/SubscriptionService.java
+++ src/main/java/tech/tennoji/dronewatchserver/service/SubscriptionService.java
@@ -3,12 +3,15 @@ package tech.tennoji.dronewatchserver.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import tech.tennoji.dronewatchserver.entity.FenceStatus;
 import tech.tennoji.dronewatchserver.entity.GeoFence;
 import tech.tennoji.dronewatchserver.entity.Subscription;
 import tech.tennoji.dronewatchserver.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,7 +21,10 @@ public class SubscriptionService {
     @Autowired
     private MongoRepository mongoRepository;
 
-    public int subscribeTopic(String token, String topic) {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public int subscribeToTopic(String token, String topic) {
         try {
             mongoRepository.addSubscription(new Subscription(token, topic));
             return 0;
@@ -28,18 +34,33 @@ public class SubscriptionService {
         }
     }
 
-    public void unsubscribeTopic(String token, String topic) {
+    public void unsubscribeToTopic(String token, String topic) {
         long count = mongoRepository.removeSubscription(token, topic);
     }
 
     public List<String> getSubscribedTopics(String token) {
         var subscriptions = mongoRepository.findSubscriptionByToken(token);
-        return subscriptions.parallelStream().map(Subscription::getTopic).collect(Collectors.toList());
+        return subscriptions.stream().map(Subscription::getTopic).collect(Collectors.toList());
+    }
+
+    public List<FenceStatus> getSubscribedAreaStatus(String token) {
+        List<FenceStatus> statusList = mongoRepository.findSubscriptionByToken(token).stream().map(
+                subscription -> {
+                    FenceStatus status = new FenceStatus();
+                    status.setName(subscription.getTopic());
+                    return status;
+                }
+        ).collect(Collectors.toList());
+        statusList.forEach(status -> {
+            Long num = redisTemplate.opsForSet().size(status.getName());
+            status.setNumber(Objects.requireNonNullElse(num, 0).longValue());
+        });
+        return statusList;
     }
 
     public List<String> getNotSubscribedTopics(String token) {
-        var allTopics = mongoRepository.findAllAreas().parallelStream().map(GeoFence::getName).collect(Collectors.toList());
-        var subscribedTopics = mongoRepository.findSubscribersByTopic(token).parallelStream().map(Subscription::getTopic).collect(Collectors.toList());
+        var allTopics = mongoRepository.findAllAreas().stream().map(GeoFence::getName).collect(Collectors.toList());
+        var subscribedTopics = mongoRepository.findSubscribersByTopic(token).stream().map(Subscription::getTopic).collect(Collectors.toList());
         allTopics.removeAll(subscribedTopics);
         return allTopics;
     }
